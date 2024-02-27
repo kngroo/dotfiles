fae5bd2a8a0af21e8c94348492f0c59c62d2c381
Josh <3375278+kngroo@users.noreply.github.com>
2024-02-27 13:02:37 -0800

    Install and configure p10k

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -2,3 +2,5 @@
 
 # Install ohmyzsh with the unattended flag: https://github.com/ohmyzsh/ohmyzsh?tab=readme-ov-file#unattended-install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
