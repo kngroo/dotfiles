b0f9fc0a3c414b4f38344d41cf077596a9dba66b
Josh <3375278+kngroo@users.noreply.github.com>
2024-02-28 14:36:45 -0800

    Add install script for fzf-tab

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -3,4 +3,8 @@
 # Install ohmyzsh with the unattended flag: https://github.com/ohmyzsh/ohmyzsh?tab=readme-ov-file#unattended-install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Install the powerlevel10k prompt
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install the fzf-tab plugin
+git clone --depth=1 https://github.com/Aloxaf/fzf-tab.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab
